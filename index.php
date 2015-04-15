445f26a4f27817d957374aa090517ababa5e30bd
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-14 21:30:50 -0700

    Milestone 1 and 2: - Created folder for project called "todo-list" - Created index.php file for Project

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> To-DO List</title>
+	<!-- links to css file in css folder-->
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+	
+	</div>
+</body>
+</html>
