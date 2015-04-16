64953db52110c66ed935924a93625f370e11ebc2
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-15 20:17:27 -0700

    Milestone 10: - Fixed Function Errors

--- index.php
+++ index.php
@@ -27,8 +27,8 @@
 			var new_task = $('.add-new-task input[name=new-task').val();
 
 			if (new_task != '') {
-				$.post('includes/add-task.php', {task: new_task}, function{data} {
-					$(('add-new-task input[name=new-task']).val();
+				$.post('includes/add-task.php', {task: new_task}, function(data) {
+					$('add-new-task input[name=new-task']).val();
 						$(data).appendTo('task-list ul').hide().fadeIn();)
 				});
 			}
