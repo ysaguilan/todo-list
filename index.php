e0e259ab69367f0069d7fc7d70794852b196a048
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-15 20:14:37 -0700

    Milestones 8-9: - Added A Submit Task Function - Added/Used Jquery And Javascript Scripts

--- index.php
+++ index.php
@@ -18,4 +18,22 @@
 		</form>
 	</div>
 </body>
+<script src="http://code.jquery.com/jquery-lastest.min.js"></script>
+<script>
+	add_task(); // calling add task function
+
+	function add_task() {
+		$('add-new-task').submit(function() {
+			var new_task = $('.add-new-task input[name=new-task').val();
+
+			if (new_task != '') {
+				$.post('includes/add-task.php', {task: new_task}, function{data} {
+					$(('add-new-task input[name=new-task']).val();
+						$(data).appendTo('task-list ul').hide().fadeIn();)
+				});
+			}
+			return false;
+		});
+	}
+</script>
 </html>
