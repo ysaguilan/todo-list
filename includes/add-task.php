f3976d5d370de3b1ed9fb3b86a56aa57e8b8a039
yulitza saguilan <yulitza.saguilan@gmail.com>
2015-04-16 09:17:07 -0700

    Milestone 13: - Added If Function To Take In Results From Query - Added Echo To Echo Out A Delete Button

--- includes/add-task.php
+++ includes/add-task.php
@@ -11,3 +11,14 @@ $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 $mysqli-> query("INSERT INTO tasks VALUES('', '$tasks', '$date', '$time')");
 //queriies tasks that we have; will allow us to go through it
 $query = "SELECT * FROM tasks WHERE task='$task' and date='$date' and time='$time'"
+//takes results from query
+if ($result = $mysqli->query($query)) {
+	while ($row = $result->fetch_associ()) {
+		$task_id = $row['id'];
+		$task_name = $row['task'];
+	}
+}
+//closes connection
+$mysqli->close();
+
+echo '<li><span>' . $task_name . '</span><img id="' . $task_id. '" class="delete-button" width="10px" src="images/close.svg" /></li>';
